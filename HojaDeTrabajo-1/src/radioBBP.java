9aec307411389e01ac0411b11384be0b5de45fda
boggdan95 <bandabog@gmail.com>
2015-07-12 12:25:16 -0600

    Se ha creado la clase radioBBP que es el radio a modelar.

---
+++ HojaDeTrabajo-1/src/radioBBP.java
@@ -0,0 +1,83 @@
+/**
+ * 
+ */
+
+/**
+ * @author Boggdan Barrientos, Bryan Chan, Pablo Arreola
+ *
+ */
+public class radioBBP implements radio {
+
+	/* (non-Javadoc)
+	 * @see radio#encenderApagar()
+	 */
+	@Override
+	public boolean encenderApagar() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see radio#cambiarFrecuencia()
+	 */
+	@Override
+	public String cambiarFrecuencia() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see radio#cambiarEmisora()
+	 */
+	@Override
+	public double cambiarEmisora() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see radio#adelantarEmisora()
+	 */
+	@Override
+	public double adelantarEmisora() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see radio#retrocederEmisora()
+	 */
+	@Override
+	public double retrocederEmisora() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see radio#guardarEmisora()
+	 */
+	@Override
+	public double guardarEmisora() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see radio#subirVolumen()
+	 */
+	@Override
+	public int subirVolumen() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see radio#bajarVolumen()
+	 */
+	@Override
+	public int bajarVolumen() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
