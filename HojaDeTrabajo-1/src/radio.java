5d3cd7a65de7b5414f3c23ae512cd94f1d4ef450
boggdan95 <bandabog@gmail.com>
2015-07-12 12:00:03 -0600

    Interface creada.

--- HojaDeTrabajo-1/src/radio.java
+++ HojaDeTrabajo-1/src/radio.java
@@ -1,4 +1,19 @@
 
 public interface radio {
-
+	
+	public boolean encenderApagar();
+	
+	public String cambiarFrecuencia ();
+	
+	public double cambiarEmisora ();
+	
+	public double adelantarEmisora();
+	
+	public double retrocederEmisora();
+	
+	public double guardarEmisora();
+	
+	public int subirVolumen();
+	
+	public int bajarVolumen();
 }
